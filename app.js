20f7f979f69fb94ae2283a78c3dd2080255c430c
jensakrom <jensakrom@gmail.com>
2019-02-11 17:11:28 +0700

    create Rest API wit node.js

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next ) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Accept, Authorization');
+  if (req.methode === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methodes', 'PUT, POST, PATCH, DELETE');
+    return res.status(200).json({})
+  }
+  next();
+});
+
+//Route
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next ) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error)
+
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message
+    }
+  })
+});
+
+module.exports = app;
