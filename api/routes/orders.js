e36f71f092bda500097336769b372fedac808069
jensakrom <jensakrom@gmail.com>
2019-02-14 16:56:11 +0700

    Populate Queries Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ const Product = require("../models/product");
 router.get('/', (req, res, next) => {
   Order.find()
       .select('product quantity _id')
+      .populate('product', 'name')
       .exec()
       .then(docs => {
         res.status(200).json({
@@ -76,6 +77,7 @@ router.post('/', (req, res, next) => {
 router.get('/:orderId', (req, res, next) => {
  Order.findById(req.params.orderId)
      .exec()
+     .populate('product')
      .then(order => {
        if (!order) {
          res.status(404).json({
